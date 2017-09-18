9531190d90a975fae0a67c444b44790d0d695f0b
dd <ddresearch@shaw.ca>
2017-09-18 11:03:24 -0700

    Lesson 19

---
+++ Smack/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by D D on 2017-09-18.
+//  Copyright © 2017 ddresearch. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+    
+}
---
+++ Smack/MessageService.swift
@@ -0,0 +1,44 @@
+//
+//  MessageService.swift
+//  Smack
+//
+//  Created by D D on 2017-09-18.
+//  Copyright © 2017 ddresearch. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannels(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                
+                if let json = JSON(data: data).array {
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    
+                    print (self.channels)
+                    completion(true)
+                }
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
